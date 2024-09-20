27046f51035f27d9feddfa1d1ffa64838b01017c
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:09 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/unzipport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/unzipport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/unzipport.git'), string(name: 'PORT_DESCRIPTION', value: 'unzip utility' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/unzipport.git'), string(name: 'PORT_DESCRIPTION', value: 'unzip utility' )]
   }
 }
